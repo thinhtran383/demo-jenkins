aca29fdf683f6439f9e9d5c61374623f3087667d
ThinhTran <thinhtran3803@gmail.com>
2025-03-07 14:53:15 +0700

    init project

---
+++ src/main/java/online/thinhtran/jenkinsdemo/controller/HomeController.java
@@ -0,0 +1,16 @@
+package online.thinhtran.jenkinsdemo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public ResponseEntity<?> sayHello() {
+        return ResponseEntity.ok("Hello Jenkins");
+    }
+}
